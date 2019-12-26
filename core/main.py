b191199648882fe657417142d688cc77de04809e
Victor Kataev <victor.kataev@gmail.com>
2019-12-26 17:17:09 +0100

    Created function main()

--- core/main.py
+++ core/main.py
@@ -4,22 +4,25 @@ import sys
 import utils
 from config import *
 
+def main():
+    img = Image.open(sys.argv[-2])
+    matrix = np.asarray(img, np.uint8)
 
-img = Image.open(sys.argv[-2])
-matrix = np.asarray(img, np.uint8)
+    args_list = [x for x in sys.argv[1:-2] if '--' in x]
 
-args_list = [x for x in sys.argv[1:-2] if '--' in x]
+    for opt in args_list:
+        opt = opt[2:] # discard leading --
+        val = getattr(args, opt)
+        if val not in [None, False]:
+            func = getattr(utils, opt)
+            matrix = func(matrix, val)
 
-for opt in args_list:
-    opt = opt[2:] # discard leading --
-    val = getattr(args, opt)
-    if val not in [None, False]:
-        func = getattr(utils, opt)
-        matrix = func(matrix, val)
+    if matrix.ndim == 3:
+        output = Image.fromarray(matrix.astype(dtype=np.uint8), mode='RGB')
+    else:
+        output = Image.fromarray(matrix.astype(dtype=np.uint8), mode='L')
+    output.save(args.dst)
 
-if matrix.ndim == 3:
-    output = Image.fromarray(matrix.astype(dtype=np.uint8), mode='RGB')
-else:
-    output = Image.fromarray(matrix.astype(dtype=np.uint8), mode='L')
-output.save(args.dst)
 
+if __name__ == '__main__':
+    main()
